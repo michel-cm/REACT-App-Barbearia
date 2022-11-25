b372a33c6629728e8a670215fd6ef661e84e2053
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-11-25 08:07:29 -0300

    Delete src/hooks directory

--- src/hooks/useAssistidosContext.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { AssistidosContext } from "../contexts/AssistidosContext";
-
-export function useAssistidosContext() {
-  const value = useContext(AssistidosContext);
-
-  return value;
-}
